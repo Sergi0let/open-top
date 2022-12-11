9ea380df0117f45ba24b1a3f59250645f1aa9ef1
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2022-12-11 11:38:02 +0200

    header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import Head from 'next/head';
+
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My top</title>
+        <link rel="icon" href="/favicon_2.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
